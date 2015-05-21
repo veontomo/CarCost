f64da75d513f8fd816e0dbf27a17bfbc8a5125ba
Mario Rossi <veontomo@gmail.com>
2015-05-21 21:09:24 +0200

    feat: add Station Activity

--- app/src/main/java/com/veontomo/carcost/StatisticsActivity.java
+++ app/src/main/java/com/veontomo/carcost/StatisticsActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class RefuelStatistics extends ActionBarActivity {
+public class StatisticsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
