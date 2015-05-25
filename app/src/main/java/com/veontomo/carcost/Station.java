84a6774246f40e104a5db2b3121fb8d3c960026c
Mario Rossi <veontomo@gmail.com>
2015-05-25 22:25:21 +0200

    feat: create layout for adding car

--- app/src/main/java/com/veontomo/carcost/Station.java
+++ app/src/main/java/com/veontomo/carcost/Station.java
@@ -7,11 +7,24 @@ package com.veontomo.carcost;
  * @since 0.1
  */
 public class Station {
-    private String descr;
+    private String distributor;
     private String name;
-    private Float rating;
+    private String country;
+    private String street;
+    private String building;
+    private Float latitude;
+    private Float longitude;
 
-    public void Station(String name){
+
+    public void Station(String name, String distributor, String country, String street, String building){
         this.name = name;
+        this.distributor = distributor;
+        this.country = country;
+        this.street = street;
+        this.building = building;
+    }
+
+    public void save(){
+
     }
 }
